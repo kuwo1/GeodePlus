8b30c4531f1dbd517be2aed9a237deb273353e60
kuwo1 <rvkeely2.0@gmail.com>
2026-06-10 00:14:54 -0500

    idk what i'm doing here goes something

---
+++ src/main/java/net/kuwo1/geodeplus/Geode.java
@@ -0,0 +1,16 @@
+package net.kuwo1.geodeplus;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Geode implements ModInitializer {
+	public static final String MOD_ID = "geode";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/kuwo1/geodeplus/GeodeDataGenerator.java
@@ -0,0 +1,11 @@
+package net.kuwo1.geodeplus;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GeodeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
