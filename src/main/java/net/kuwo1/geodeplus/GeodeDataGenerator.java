1951b50cd3b1feba23bb60296868ca199829e94a
kuwo1 <rvkeely2.0@gmail.com>
2026-06-15 00:28:34 -0500

    it's broken again :/

--- src/main/java/net/kuwo1/geodeplus/GeodeDataGenerator.java
+++ src/main/java/net/kuwo1/geodeplus/GeodeDataGenerator.java
@@ -2,10 +2,8 @@ package net.kuwo1.geodeplus;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-import net.kuwo1.geodeplus.datagen.ModBlockLootTableProvider;
-import net.kuwo1.geodeplus.datagen.ModBlockTagsProvider;
-import net.kuwo1.geodeplus.datagen.ModModelProvider;
-import net.kuwo1.geodeplus.datagen.ModRecipeProvider;
+import net.kuwo1.geodeplus.datagen.*;
+import net.kuwo1.geodeplus.world.feature.ModFeatures;
 
 public class GeodeDataGenerator implements DataGeneratorEntrypoint {
 	@Override
@@ -16,5 +14,6 @@ public class GeodeDataGenerator implements DataGeneratorEntrypoint {
         pack.addProvider(ModBlockTagsProvider::new);
         pack.addProvider(ModBlockLootTableProvider::new);
         pack.addProvider(ModRecipeProvider::new);
+        pack.addProvider(ModFeatures::new);
 	}
 }
---
+++ src/main/java/net/kuwo1/geodeplus/world/feature/ModFeatures.java
@@ -0,0 +1,29 @@
+package net.kuwo1.geodeplus.world.feature;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModFeatures {public class GeodeWorldgenProvider extends FabricDynamicRegistryProvider {
+    public GeodeWorldgenProvider(FabricPackOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void configure(HolderLookup.Provider registries, Entries entries) {
+        entries.addAll(registries.lookupOrThrow(Registries.CONFIGURED_FEATURE));
+        entries.addAll(registries.lookupOrThrow(Registries.PLACED_FEATURE));
+    }
+
+    @Override
+    public String getName() {
+        return "GeodePlus World Generation";
+    }
+
+
+
+}
+}
